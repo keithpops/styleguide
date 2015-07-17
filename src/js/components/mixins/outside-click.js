8baecc42c0ab5bdcb657075d5657eaf60da07117
Keith Poplawski <keith.poplawski@gmail.com>
2015-07-17 17:35:15 -0400

    converts outside click to regs es6 class

--- src/js/components/mixins/outside-click.js
+++ src/js/components/mixins/outside-click.js
@@ -1,18 +1,24 @@
 import React from 'react';
 
 const {
-  PropTypes : Type
+  findDOMNode,
+  Component,
 } = React;
 
-export default {
+class OutsideClick extends Component {
+
+  constructor(props) {
+    super(props);
+    this._handleDocumentClick = this._handleDocumentClick.bind(this);
+  }
 
   componentDidMount() {
     window.addEventListener('click', this._handleDocumentClick);
-  },
+  }
 
   componentWillUnmount() {
     window.removeEventListener('click', this._handleDocumentClick);
-  },
+  }
 
   _handleDocumentClick(e) {
     const target = e.target;
@@ -28,4 +34,14 @@ export default {
       }
     }
   }
+
+  render() {
+    return this.props.children;
+  }
 }
+
+OutsideClick.propTypes = {
+  onClick: React.PropTypes.func.isRequired,
+};
+
+export default OutsideClick;
