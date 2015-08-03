20e28cd825edeb7d1b701a3589824a07c750d5d6
Keith Poplawski <keith.poplawski@gmail.com>
2015-08-03 15:43:28 -0400

    sets prop types for form component

--- src/js/components/forms/form.js
+++ src/js/components/forms/form.js
@@ -3,11 +3,25 @@ import getFormData from 'get-form-data';
 
 const {
   createClass,
+  PropTypes,
 } = React;
 
 
 const Form = createClass({
 
+  propTypes: {
+    onChange: PropTypes.func,
+    onSubmit: PropTypes.func,
+    trim: PropTypes.bool,
+    trimOnSubmit: PropTypes.bool
+  },
+
+  getDefaultProps() {
+    return {
+      trim: false,
+      trimOnSubmit: false
+    }
+  },
 
   render() {
 
