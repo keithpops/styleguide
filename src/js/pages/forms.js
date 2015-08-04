ac3e8929d5d86fafe64a62b9fbc4f079ea80a1ef
Keith Poplawski <keith.poplawski@gmail.com>
2015-08-04 11:20:59 -0400

    removes extra testing divs

--- src/js/pages/forms.js
+++ src/js/pages/forms.js
@@ -65,14 +65,10 @@ export default React.createClass({
   render() {
     return <Styleguide title="Forms Styles">
         <div title="Form">
-          <Form onSubmit={this._onSubmit} onChange={this._onChange} endpoint="http://www.cnn.com">
-            <div><div><div><div>
-              <TextField name="sample-textfield" label="Text" fieldColor='light' placeholder="Placeholder" extraClasses={['py2']} />
-            </div></div></div></div>
-            <div><div><div><div>
-              <TextArea name="sample-textarea" label="Textarea" fieldColor='light'  extraClasses={['py2']} />
-              <TextArea name="sample-textarea-expandable" label="Textarea Expandable" fieldColor='light' expandable={true} extraClasses={['py2']} />
-            </div></div></div></div>
+          <Form onSubmit={this._onSubmit} onChange={this._onChange} endpoint="http://www.cnn.com" className="clearfix">
+            <TextField name="sample-textfield" label="Text" fieldColor='light' placeholder="Placeholder" extraClasses={['py2']} />
+            <TextArea name="sample-textarea" label="Textarea" fieldColor='light'  extraClasses={['py2']} />
+            <TextArea name="sample-textarea-expandable" label="Textarea Expandable" fieldColor='light' expandable={true} extraClasses={['py2']} />
           </Form>
         </div>
         <div title="Forms">
