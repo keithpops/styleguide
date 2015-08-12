7f46ff2b331a19befa9d4ae2dff50f091d4afedc
Keith Poplawski <keith.poplawski@gmail.com>
2015-08-12 13:49:01 -0400

    basic styling updates

--- src/js/components/forms/fields/slider.js
+++ src/js/components/forms/fields/slider.js
@@ -10,9 +10,9 @@ export default createClass({
 
   render() {
 
-    return <div className="flex">
+    return <div className="flex relative">
       <div className="flex-auto bg-grey-5 rounded-1" style={{height: '10px'}}></div>
-      <div className="circle bc-blue-secondary bg-blue bw-3 b" style={{height: '8px', width: '8px'}}></div>
+      <div className="circle bc-blue-secondary bg-blue bw-3 b absolute top-0 left-0" style={{height: '8px', width: '8px'}}></div>
     </div>;
   }
 });
