18d455c837c07774f3e78aad946a9f88d1df5410
Keith Poplawski <keith.poplawski@gmail.com>
2015-08-13 13:32:22 -0400

    adds paragraph component examples to style guide

--- src/js/pages/base.js
+++ src/js/pages/base.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Styleguide from '../styleguide';
 import Colors from './colors';
 import Typography from './base/typography';
+import P from '../components/paragraph';
 
 let D = React.DOM;
 
@@ -11,6 +12,15 @@ export default React.createClass({
   render() {
     return (
       <Styleguide title="Base Styles">
+        <div title="Text Components" description="React Text Components!">
+          <P>This is a paragraph</P>
+          <P size="small">This is a small paragraph</P>
+          <P align="center">This is a center-aligned paragraph</P>
+          <P align="right">This is a right-aligned paragraph</P>
+          <P size="small" align="center">This is a small, center-aligned paragraph</P>
+          <P size="small" align="right">This is a small, right-aligned paragraph</P>
+        </div>
+
         <div title="Typography" description="Typographical settings">
           <Typography />       
         </div>
